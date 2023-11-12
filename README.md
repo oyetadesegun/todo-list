9a9e60f6fa1dc085c8597af0968f93e20a46dc1f
Oyetade Segun <oyetadesegun@gmail.com>
2023-11-12 23:07:50 +0100

    add new

---
+++ README.md
@@ -0,0 +1,24 @@
+# React Todo List
+
+A simple and intuitive Todo List application built with React and JSX.
+
+## Features
+
+- Add tasks to your Todo List
+- Mark tasks as completed
+- Delete tasks from the list
+- Clear all completed tasks with a single click
+
+## Getting Started
+
+### Prerequisites
+
+Make sure you have Node.js and npm installed on your machine.
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/oyetadesegun/todo-list.git
+   ```
---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import "./styles.css";
+import NewTodoForm from "./NewTodoForm";
+export default function App() {
+function addTodo(){
+  setTodos(currentTodos =>{
+    return [
+      ...currentTodos,
+      {id:}
+    ]
+  })
+}
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
+  return (
+    <>
+      <NewTodoForm/>
+      <h1 className="header">Todo List</h1>
+
+      <ul className="list">
+        {todos.length === 0 && "No todos"}
+        {todos.map((todo) => {
+          return (
+            <>
+              <li key={todo.id}>
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={todo.completed}
+                    onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                  />
+                  {todo.title}
+                </label>
+                <button
+                  onClick={() => deleteTodo(todo.id)}
+                  className="btn btn-danger"
+                >
+                  Delete
+                </button>
+              </li>
+            </>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
